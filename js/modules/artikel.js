29f661f02a03ca308ef8535e3a6c2039ff6b15e1
Alban891 <alban.osmani@gmx.de>
2025-10-09 11:32:42 +0200

    artikel.js

--- js/modules/artikel.js
+++ js/modules/artikel.js
@@ -113,6 +113,8 @@ export function renderArtikelListByProjekt() {
   `;
 }).join('');
 
+}
+
 // ==========================================
 // ARTIKEL DETAIL VIEW
 // ==========================================
